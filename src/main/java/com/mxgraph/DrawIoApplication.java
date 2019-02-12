276e42867509897958a1b7d01ee87602f7e4726c
wuhao <wuhaotx@feinno.com>
2019-02-12 17:12:04 +0800

    can save to urcs server

--- src/main/java/com/mxgraph/DrawIoApplication.java
+++ src/main/java/com/mxgraph/DrawIoApplication.java
@@ -1,11 +1,11 @@
 package com.mxgraph;
 
+import org.helium.boot.spring.annotation.EnableHeliumConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableHeliumConfiguration
 public class DrawIoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DrawIoApplication.class, args);
--- src/main/java/com/mxgraph/control/FileControl.java
+++ src/main/java/com/mxgraph/control/FileControl.java
@@ -1,6 +1,12 @@
 package com.mxgraph.control;
 
+import com.alibaba.fastjson.JSONObject;
+import com.feinno.superpojo.util.StringUtils;
+import com.feinno.util.DateUtil;
 import com.mxgraph.utils.HttpUtil;
+import com.urcs.data.service.common.content.ContentBean;
+import com.urcs.data.service.common.content.ContentService;
+import org.helium.framework.annotations.ServiceSetter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +15,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.UUID;
 
 /**
  *
@@ -17,6 +24,12 @@ import java.io.IOException;
 @RestController
 @RequestMapping("/file")
 public class FileControl {
+
+    @ServiceSetter
+    private ContentService contentService;
+
+    private String type = "drawview";
+
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     /**
@@ -31,14 +44,33 @@ public class FileControl {
     @ResponseBody
     public void get(@PathVariable String flid, HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-        String body = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-                "\n" +
-                "<mxfile modified=\"2019-02-12T07:07:12.236Z\" host=\"127.0.0.1\" agent=\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.96 Safari/537.36\" etag=\"Hxi9cdVr-Ee_9CzLO883\" version=\"10.0.39\" type=\"browser\">\n" +
-                "  <diagram id=\"FO2v3a15tLzoUDuactzV\" name=\"Page-1\">zZZNc6owFIZ/DUtngIjYpYC1tTr9cKZXV50UIqQNBEMo0F/feAkKYqcuHHRF8p6TD57zJhMF2GE+YTAO5tRDRNFVL1eAo+i61geq+GyVolRMDZSCz7Ank/bCAn8jKcpxfoo9lDQSOaWE47gpujSKkMsbGmSMZs20NSXNVWPoo5awcCFpq/+wx4NSHermXr9D2A+qlbXBTRkJYZUs/yQJoEezmgTGCrAZpbxshbmNyBZexaUcd/tLdLcxhiJ+ygBjRfXifukx+I7h0l0O43nak7N8QZLKH5ab5UVFQMwiYIuOlQWYo0UM3W0kE/UWWsBDInqaaMIkLiuwxjkSi1rtHVbLIcZRXpPkjieIhoizQqTIKKjoFZWhZD+rFUNKQa0OlQZl+f3dzHtCoiEhHQcGXcdLljxntjFZvVjTF/sBXD0w7RDY8MLA9L+BiZMRb5sxoy5Kkr/BvUP302c0jbzHlBMcIamfg5/R5Ge08e0Q1/kNzsDPJ9YmJTOWptnrjD4YheOOrt5wffPAcGp3hntC69k6T5JglhM8tkbBZj49Bdg1GU5Xm/x2TurCcUcBnnBiL+q4wxuuQ8MNTccf97+nn4NVz07UCBXOxymGuwAlQzvwldkdJu3WeWb3z5iYm9eR8THvvRk3XVxkLTpHGP5uK9AE1j9yEM8ETHT3777/sdrrGYx/AA==</diagram>\n" +
-                "</mxfile>\n";
-        LOGGER.info("get File:{}", body);
-        response.getOutputStream().write(body.getBytes());
-        response.setHeader("title", System.currentTimeMillis() + ".xml");
+        ContentBean contentBean = new ContentBean();
+        //为空生成id
+        if (StringUtils.isNullOrEmpty(flid)){
+            flid = UUID.randomUUID().toString();
+        }
+        //默认名称
+        String title = flid + ".xml";
+        contentBean.setType(type);
+        contentBean.setCns(type);
+        contentBean.setName(title);
+        contentBean.setId(flid);
+
+        //获取内容
+        contentBean = contentService.get(contentBean);
+        String xmlBody = "";
+        String xmlTitle = "";
+        if (contentBean == null){
+            xmlBody = DXML;
+            xmlTitle = title;
+            contentService.save(contentBean);
+        } else {
+            xmlBody = contentBean.getBody();
+            xmlTitle = contentBean.getName();
+        }
+        LOGGER.info("get File:{}-{}", xmlTitle, xmlBody);
+        response.getOutputStream().write(xmlBody.getBytes());
+        response.setHeader("title", title);
         response.setStatus(200);
     }
 
@@ -46,9 +78,26 @@ public class FileControl {
     @PostMapping("/save")
     @ResponseBody
     public void save(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
         String body = HttpUtil.getBodyString(request);
-        System.out.println(body);
-        LOGGER.info("save File:{}", body);
+        String xmlTitle = request.getHeader("title");
+        String xmlId = request.getHeader("flid");
+
+        ContentBean contentBean = new ContentBean();
+        contentBean.setType(type);
+        contentBean.setCns(type);
+        contentBean.setBody(body);
+        contentBean.setName(xmlTitle);
+        contentBean.setId(xmlId);
+        contentBean.setModifyTime(DateUtil.getDefaultYearMonthDayTime());
+
+        contentService.save(contentBean);
+        LOGGER.info("save File:{}-{}-{}", xmlTitle, body);
         response.setStatus(200);
     }
+
+    private static final String DXML = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+            "<mxfile modified=\"2019-02-12T08:55:28.159Z\" host=\"127.0.0.1\" agent=\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.96 Safari/537.36\" etag=\"BhcgNWRefkg5PAe-CUDU\" version=\"10.0.39\" type=\"browser\">\n" +
+            "  <diagram id=\"FO2v3a15tLzoUDuactzV\" name=\"Page-1\">dZHBEoIgEIafhrtCk3U2y0snD50Z2YAZdB2k0Xr6dMCMsU4s3/4/y+4SljfjxfJOXVGAITQRI2EnQmm6Y8l0zOTpSZYyD6TVIohWUOkXBBh88qEF9JHQIRqnuxjW2LZQu4hxa3GIZXc0cdWOS9iAquZmS29aOOXpgWYrL0FLtVRO90efafgiDp30igscvhArCMstovNRM+Zg5uEtc/G+85/s52MWWvfDMAXr29Ml2hAr3g==</diagram>\n" +
+            "</mxfile>\n";
 }
