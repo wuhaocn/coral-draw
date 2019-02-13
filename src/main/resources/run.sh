efda183eaa31ebba4aabf4a73897366e88575d39
wuhao <wuhaotx@feinno.com>
2019-02-13 18:22:50 +0800

    采用jar依赖

--- src/main/resources/run.sh
+++ src/main/resources/run.sh
@@ -5,6 +5,17 @@ ulimit -n 32768
 
 source /etc/profile
 
+cp -rf /etc/hosts /tmp/hoststmp
+sed -i '$a\HB_HOST urcs_hbase' /tmp/hoststmp
+sed -i '$a\REDIS_HOST urcs_redis' /tmp/hoststmp
+sed -i '$a\KAFKA_HOST urcs_kafka' /tmp/hoststmp
+sed -i "s/HB_HOST/$HB_HOST/g" /tmp/hoststmp
+sed -i "s/REDIS_HOST/$REDIS_HOST/g" /tmp/hoststmp
+sed -i "s/KAFKA_HOST/$KAFKA_HOST/g" /tmp/hoststmp
+echo $DB_HOST
+echo $REDIS_HOST
+cat /tmp/hoststmp
+cat /tmp/hoststmp > /etc/hosts
 
 basePath=$(cd "$(dirname "$0")";pwd)
 SERVICE_HOME=$basePath
