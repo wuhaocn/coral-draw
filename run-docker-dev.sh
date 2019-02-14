fd1d14e9b391020400fdd2acd1fd5f96ca8748e0
wuhao <wuhaotx@feinno.com>
2019-02-14 14:16:44 +0800

    支持docker部署，部署成功

--- run-docker-dev.sh
+++ run-docker-dev.sh
@@ -4,7 +4,7 @@ docker rm `docker ps -a -q`
 
 docker run --privileged=true \
            -p 8081:8081 \
-           -it --name drawio1.0 \
+           -d --name drawio1.0 \
            10.10.208.193:5000/drawio:1.0
 
 docker logs drawio1.0
