28a31286b66eafddc2de40595b4fc0d6962a4d48
wuhao <wuhaotx@feinno.com>
2019-04-01 15:38:52 +0800

    opt

--- docker-run.sh
+++ docker-run.sh
@@ -5,7 +5,7 @@ docker rm `docker ps -a -q`
 docker run --privileged=true \
            -p 8081:8081 \
            -d --name coral-draw \
-           10.10.208.193:5000/coral-draw:3.0.0-1904011522
+           10.10.208.193:5000/coral-draw:3.0.0-1904011527
 
 docker logs -f coral-draw
 
