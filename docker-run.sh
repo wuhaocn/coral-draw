ef61a9345505870e074747ea6e3d97f7bb0db352
wuhao <wuhaocn@126.com>
2020-06-26 16:13:43 +0800

    README.md

--- docker-run.sh
+++ docker-run.sh
@@ -1,12 +1,12 @@
 docker stop coral-draw
 
-docker rm `docker ps -a -q`
+docker rm coral-draw
 
 docker run --privileged=true \
            -p 8081:8082 \
            -d --name coral-draw \
-           --env DB_HOST='127.0.0.1' \
-           10.10.208.193:5000/coral-draw:3.0.0-2006261529
+           --env DB_HOST='192.168.3.10' \
+           10.10.208.193:5000/coral-draw:3.0.0-2006261540
 
 docker logs -f coral-draw
 
