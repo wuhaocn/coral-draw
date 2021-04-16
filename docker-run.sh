e887e59c1de5e2b61c0e86c944a4cd5a0d3af9d5
wuhao <wuhaocn@126.com>
2021-04-16 09:27:05 +0800

    push to dockerhub and use dockerhub  deploy

--- docker-run.sh
+++ docker-run.sh
@@ -1,13 +1,16 @@
-docker stop coral-draw
+docker stop coral-drawhubdb
+docker rm coral-drawhubdb
+docker run -d --name coral-drawhubdb -p 3306:3306 wuhaocn/mysqldrawdb:5.6.40
+docker update coral-drawhubdb --restart=always
 
-docker rm coral-draw
 
+### 创建数据库 DrawDB [root coral@2018],[数据库表自动生成]
+
+docker stop coral-drawhub
+docker rm coral-drawhub
 docker run --privileged=true \
-           -p 8081:8082 \
-           -d --name coral-draw \
-           --env DB_HOST='192.168.3.10' \
-           10.10.208.193:5000/coral-draw:3.0.0-2006261540
-
-docker logs -f coral-draw
-
-#docker exec -it fce8d10b5c31 bash
+           -p 8080:8082 \
+           -d --name coral-drawhub \
+           --env DB_HOST='10.1.0.35' \
+           wuhaocn/coral-drawhub:3.0.0-2104151418
+docker update coral-drawhub --restart=always
